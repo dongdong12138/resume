2983a1a35fe369cf0adca8887ab13b17973c723e
dongdong <286084845@qq.com>
2018-09-06 15:22:03 +0800

    更新

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
 // 页面加载成功
-siteWelcome.classList.remove('active');
+// setTimeout(function() {
+//     siteWelcome.classList.remove('active');
+// }, 2000);
+
+
+// sticky navbar
+window.onscroll = function () {
+    if (window.scrollY > 0) {
+        topNavBar.classList.add('sticky');
+    } else {
+        topNavBar.classList.remove('sticky');
+    }
+};
+
+
+// subnav
+let aTags = document.getElementsByClassName('menuTigger');
+for (let i = 0; i < aTags.length; i ++) {
+    aTags[i].onmouseenter = function (event) {
+        let a = event.currentTarget;            // 找 a 的弟弟
+        let brother = a.nextSibling;
+        while (brother.nodeType === 3) {
+            brother = brother.nextSibling;
+        }
+        console.log(brother);
+    };
+    aTags[i].onmouseleave = function (event) {};
+}
+
+
 
 // 作品集滚动条滚动
 var sampleAll = document.getElementById('sampleAll');
