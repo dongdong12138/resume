b3d787bfde239594cc2b893d7d611e27d5ffb654
dongdong <286084845@qq.com>
2018-10-25 12:12:30 +0800

    加载动画js

---
+++ js/loading.js
@@ -0,0 +1,26 @@
+
+!function () {
+    // 页面加载成功
+    // setTimeout(function() {
+    //     siteWelcome.classList.remove('active');
+    // }, 2000);
+
+
+
+    // 作品集滚动条滚动
+    // var sampleAll = document.getElementById('sampleAll');
+    // var sampleFrame = document.getElementById('sampleFrame');
+    // var sampleProto = document.getElementById('sampleProto');
+    // var sampleReelsBar = document.getElementsByClassName('sampleReelsBar')[0];
+
+    // sampleAll.onclick = function () {
+    //     sampleReelsBar.className = 'sampleReelsBar state-1';
+    // };
+    // sampleFrame.onclick = function () {
+    //     sampleReelsBar.className = 'sampleReelsBar state-2';
+    // };
+    // sampleProto.onclick = function () {
+    //     sampleReelsBar.className = 'sampleReelsBar state-3';
+    // };
+
+}.call()
