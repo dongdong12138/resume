0549186cb504c04f06e518e0250da0256521be56
dongdong <286084845@qq.com>
2018-10-29 21:24:57 +0800

    留言js

--- js/message.js
+++ js/message.js
@@ -1,78 +1,55 @@
-!function(){
-    var model = {
-      // 获取数据
-      init: function(){
-        var APP_ID = 'Od1rxG0lrchLp9pRPPyyELe2-gzGzoHsz'
-        var APP_KEY = 'jCXVMsw3GsgJsnhbO4sDOwnS'
-        AV.init({ appId: APP_ID, appKey: APP_KEY })
-      },
-      fetch: function(){ 
-        var query = new AV.Query('Message');
-        return query.find() // Promise 对象
-      },
-      // 创建数据
-      save: function(name, content){
-        var Message = AV.Object.extend('Message');
-        var message = new Message();
-        return message.save({  // Promise 对象
-          'name': name,
-          'content': content
-        })
-      }
-    }
-  
-    var view = document.querySelector('section.message')
-  
-  
-    var controller = {
-      view: null,
-      model: null,
-      messageList: null,
-      init: function(view, model){
-        this.view = view
-        this.model = model
-  
-        this.messageList = view.querySelector('#messageList')
-        this.form = view.querySelector('form')
-        this.model.init()
+!function () {
+  var model = Model({ resourceName: 'message' })
+
+  var view = View('section.message')
+
+  var controller = Controller({
+    init: function (view, controller) {
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.messageList = null,
         this.loadMessages()
-        this.bindEvents()
-      },
-      loadMessages: function(){
-        this.model.fetch().then(
-          (messages) => {
-            let array = messages.map((item)=> item.attributes )
-            array.forEach((item)=>{
-              let li = document.createElement('li')
-              li.innerText = `${item.name}: ${item.content}`
-              this.messageList.appendChild(li)
-            })
-          } 
-        )
-      },
-      bindEvents: function(){
-        this.form.addEventListener('submit', (e) => {
-          e.preventDefault()
-          this.saveMessage()
-        })
-      },
-      saveMessage: function(){
-        let myForm = this.form
-        let content = myForm.querySelector('input[name=content]').value
-        let name = myForm.querySelector('input[name=name]').value
-        this.model.save(name, content).then(function(object) {
-          let li = document.createElement('li')
-          li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-          let messageList = document.querySelector('#messageList')
-          messageList.appendChild(li)
-          myForm.querySelector('input[name=content]').value = ''
-          myForm.querySelector('input[name=name]').value = ''
-        })
-      }
-  
+    },
+    loadMessages: function () {
+      this.model.fetch().then(
+        (messages) => {
+          let array = messages.map((item) => item.attributes)
+          array.forEach((item) => {
+            let li = document.createElement('li')
+            li.innerText = `${item.name}: ${item.content}`
+            this.messageList.appendChild(li)
+          })
+        }
+      )
+    },
+    bindEvents: function () {
+      
+      this.form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        this.saveMessage()
+      })
+    },
+    saveMessage: function () {
+      let myForm = this.form
+      let content = myForm.querySelector('input[name=content]').value
+      let name = myForm.querySelector('input[name=name]').value
+      this.model.save({
+        'name': name,
+        'content': content
+      }).then(function (object) {
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        myForm.querySelector('input[name=content]').value = ''
+        myForm.querySelector('input[name=name]').value = ''
+      })
     }
-  
-    controller.init(view, model)
-  
-  
-  }.call()
+
+  })
+
+
+  controller.init(view, model)
+
+
+}.call()
