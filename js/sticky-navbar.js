6fb7c94067f7705fcaaf47efa17073c89ea426f7
dongdong <286084845@qq.com>
2018-10-29 21:25:18 +0800

    导航栏js

--- js/sticky-navbar.js
+++ js/sticky-navbar.js
@@ -1,7 +1,25 @@
 
 !function () {
+    var model = {
+        init() {
+            var APP_ID = 'Od1rxG0lrchLp9pRPPyyELe2-gzGzoHsz'
+            var APP_KEY = 'jCXVMsw3GsgJsnhbO4sDOwnS'
+            AV.init({ appId: APP_ID, appKey: APP_KEY })
+        },
+        fetch() {
+            var query = new AV.Query('X');
+            return query.find() // Promise 对象
+        },
+        save: function (name) {
+            var Message = AV.Object.extend('X');
+            var message = new Message();
+            return message.save({  // Promise 对象
+                'name': name
+            })
+        }
+    }
     // sticky navbar
-    var view = document.querySelector('#topNavBar')
+    var view = View('#topNavBar')
     var controller = {
         view: null,
         init: function (view) {
